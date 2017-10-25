9661f9ce8773036e1c4bd0e9cdce23155e3ad811
Samuel Mortenson <samuel.mortenson@acquia.com>
2017-10-25 10:45:42 -0700

    Handle edge case in list.sh

--- scripts/list.sh
+++ scripts/list.sh
@@ -13,20 +13,27 @@ OUTPUT="NAME RUNNING URL CPU MEMORY CODEBASE"
 for INSTANCE in $INSTANCES; do
   NAME=$INSTANCE
   CONTAINER=$(docker ps -a -q --filter "label=drupaldockerlite" --filter "label=com.docker.compose.project=$NAME" --filter "name=php")
+  if [[ "$CONTAINER" ]]; then
   RUNNING=$(docker inspect -f {{.State.Running}} $CONTAINER)
-  if [[ $RUNNING = "true" ]]; then
-    URL=$(./url.sh "$NAME")
-    if [ $? -ne 0 ]; then
+    if [[ $RUNNING = "true" ]]; then
+      URL=$(./url.sh "$NAME")
+      if [ $? -ne 0 ]; then
+        URL="n/a"
+      fi
+    else
       URL="n/a"
     fi
+    CODEBASE=$(docker container inspect --format '{{ range .Mounts }}{{ if eq .Destination "/var/www/html" }}{{ .Source }}{{ end }}{{ end }}' "$CONTAINER")
+    if [[ ! -d "$CODEBASE" ]]; then
+      CODEBASE="removed"
+    fi
+    STATS=$(docker stats -a --no-stream --format '{{ .CPUPerc }} {{ .MemPerc }}' "$CONTAINER")
   else
     URL="n/a"
+    RUNNING="n/a"
+    STATS="0% 0%"
+    CODEBASE="n/a"
   fi
-  CODEBASE=$(docker container inspect --format '{{ range .Mounts }}{{ if eq .Destination "/var/www/html" }}{{ .Source }}{{ end }}{{ end }}' "$CONTAINER")
-  if [[ ! -d "$CODEBASE" ]]; then
-    CODEBASE="removed"
-  fi
-  STATS=$(docker stats -a --no-stream --format '{{ .CPUPerc }} {{ .MemPerc }}' "$CONTAINER")
   OUTPUT="${OUTPUT}"$'\n'"$NAME $RUNNING $URL $STATS $CODEBASE"
 done
 
