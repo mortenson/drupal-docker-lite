c2d23cde3252dfb903729ae486a0a8981757f36c
Samuel Mortenson <samuel.mortenson@acquia.com>
2018-01-24 07:31:00 -0800

    Use Drush Launcher's DRUSH_LAUNCHER_FALLBACK environmental variable.

--- scripts/drush.sh
+++ scripts/drush.sh
@@ -2,11 +2,4 @@
 
 URL=$($DDL url)
 
-# Check if a site local Drush exists, and is executable.
-DRUSH=$(find code -name drush -type f -perm +111 | head -n 1)
-if [[ "$DRUSH" ]]; then
-  $DDL exec drush --root="/var/www/html/docroot" -l $URL "$@"
-# Otherwise, use Drush 8.1.15 as a fallback.
-else
-  $DDL exec drush8 --root="/var/www/html/docroot" -l $URL "$@"
-fi
+$DDL exec drush --root="/var/www/html/docroot" -l $URL "$@"
