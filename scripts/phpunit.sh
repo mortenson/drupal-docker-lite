1b17eaa258d1a535eb492ef8c5c612ea5c96f272
Samuel Mortenson <samuel.mortenson@acquia.com>
2018-01-25 11:05:33 -0800

    Support flags and options in the phpunit script.

--- scripts/phpunit.sh
+++ scripts/phpunit.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# We don't use ""$DDL exec" because we need to run in detached mode.
+# We don't use "$DDL exec" because we need to run in detached mode.
+$DDL exec pkill phantomjs
 docker exec -d -i $(docker-compose ps -q php) phantomjs --ssl-protocol=any --ignore-ssl-errors=true ./docroot/vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768
 
-sleep 3
+# Auto-magically convert test paths relative to the current directory on the
+# host to relative paths in the container.
+TEST_PATH="${!#}"
+TEST_DIR=$(cd $ORIGINAL_PWD && cd `dirname $TEST_PATH` && pwd)
+TEST=${TEST_DIR/$PWD\/code/.}/$(basename $TEST_PATH)
 
-TEST_DIR=$(cd $ORIGINAL_PWD && cd `dirname $1` && pwd)
-TEST=${TEST_DIR/$PWD\/code/.}/$(basename $1)
-
-$DDL exec ./docroot/vendor/bin/phpunit -c ./docroot/core $TEST
+$DDL exec ./docroot/vendor/bin/phpunit -c ./docroot/core "${@:0:$#}" $TEST
 
 $DDL exec pkill phantomjs
